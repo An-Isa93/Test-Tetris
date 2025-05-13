133e752ce7b919995326c97cbf62552a9b725450
Valeria Rodriguez <145387992+ValeriaNaige@users.noreply.github.com>
2025-05-12 19:31:22 -0600

    Graphs update

--- graphs.py
+++ graphs.py
@@ -31,19 +31,28 @@ plt.savefig("models/accuracy_plot.png")
 plt.show()
 
 #Confusion Matrix
-def graph_matrix(y_true, y_pred, tokenizer):
-    y_pred_classes = np.argmax(y_pred, axis=-1)
-    y_true_classes = np.argmax(y_true, axis=-1)
-    cm = confusion_matrix(y_true_classes.flatten(), y_pred_classes.flatten())
+label_names = ['R', 'L', 'r', 'd', 'D', 'C']
+label_to_index = {label: i for i, label in enumerate(label_names)}
+index_to_label = {i: label for label, i in label_to_index.items()}
+def graph_matrix(y_true, y_pred):
+    y_pred_classes = np.argmax(y_pred, axis=-1).flatten()
+    y_true_classes = np.argmax(y_true, axis=-1).flatten()
+    labels = list(index_to_label.keys())
+    label_names = [index_to_label[i] for i in labels]
+    cm = confusion_matrix(y_true_classes, y_pred_classes, labels=labels)
+
     plt.figure(figsize=(8, 6))
-    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=tokenizer.word_index.keys(), yticklabels=tokenizer.word_index.keys())
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues',
+                xticklabels=label_names, yticklabels=label_names)
     plt.xlabel('Predictions')
     plt.ylabel('True Values')
     plt.title('Confusion Matrix')
+    plt.tight_layout()
     plt.savefig("models/confusion_matrix.png")
     plt.show()
+
 
 tokenizer = joblib.load("models/tokenizer.pkl")
 y_test_classes = np.load('models/y_test.npy')
 y_pred_classes = np.load('models/y_pred.npy')
-graph_matrix(y_test_classes, y_pred_classes, tokenizer)
+graph_matrix(y_test_classes, y_pred_classes)
